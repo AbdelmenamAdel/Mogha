c31d0998c4f3e5e95092ecf3cc5734e0316d0afe
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-03-23 06:22:33 +0200

    postWidget and ReactionButton

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -18,6 +18,7 @@ class HomeViewBody extends StatelessWidget {
 
            SliverToBoxAdapter(child: CustomAppBar(),),
            SliverToBoxAdapter(child: StoryListView(),),
+           SliverToBoxAdapter(child: PostWidget(),),
 
          ],
         ),
@@ -85,5 +86,229 @@ class StoryListView extends StatelessWidget {
         ),
       ),
     );
+  }
+}
+
+class PostWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+            ),
+            title: Text('John Doe'),
+            subtitle: Text('2 hours ago'),
+            trailing: PopupMenuButton<String>(
+              itemBuilder: (BuildContext context) {
+                return ['Edit', 'Delete', 'Report'].map((String choice) {
+                  return PopupMenuItem<String>(
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'This is a sample post text with rich text formatting, @mentions, #hashtags, and location tagging.',
+              style: TextStyle(fontSize: 18.0),
+            ),
+          ),
+          Wrap(
+            spacing: 8.0,
+            runSpacing: 8.0,
+            children: [
+              Image.network('https://via.placeholder.com/150'),
+              Image.network('https://via.placeholder.com/150'),
+              Image.network('https://via.placeholder.com/150'),
+            ],
+          ),
+          SizedBox(height: 10),
+          Divider(),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 8.0),
+            child: Stack(
+              alignment: Alignment.centerLeft,
+              children: [
+                Row(
+                  children: [
+                    Text(
+                      '123',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                    SizedBox(width: 12),
+                    FacebookReactionButton(),
+                  ],
+                ),
+                ButtonBar(
+                  alignment: MainAxisAlignment.end,
+                  children: [
+                    IconButton(
+                      icon: Icon(Icons.comment),
+                      onPressed: () {},
+                    ),
+                    Text(
+                      '45',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Divider(),
+          ListTile(
+            leading: CircleAvatar(
+              backgroundImage: NetworkImage('https://via.placeholder.com/150'),
+            ),
+            title: TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Write a comment...',
+                  hintStyle: TextStyle(fontSize: 16)),
+            ),
+            trailing: IconButton(
+              icon: Icon(Icons.send),
+              onPressed: () {},
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FacebookReactionButton extends StatefulWidget {
+  @override
+  _FacebookReactionButtonState createState() => _FacebookReactionButtonState();
+}
+
+class _FacebookReactionButtonState extends State<FacebookReactionButton>
+    with SingleTickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _scaleAnimation;
+  bool _showReactions = false;
+  String _selectedReaction = '';
+
+  List<String> _reactions = [
+    '😀', // Smile
+    '😍', // Heart eyes
+    '😆', // Grinning squinting face
+    '😲', // Astonished face
+    '😢', // Crying face
+    '😠', // Angry face
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _animationController = AnimationController(
+      vsync: this,
+      duration: Duration(milliseconds: 300),
+    );
+    _scaleAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+      CurvedAnimation(parent: _animationController, curve: Curves.ease),
+    );
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      // mainAxisSize: MainAxisSize.min,
+      alignment: Alignment.centerLeft,
+      children: [
+        GestureDetector(
+          onLongPress: () {
+            _showReactions ? _hideReactions() : _showReactionsMenu();
+          },
+          child: CircleAvatar(
+            radius: 24
+            ,backgroundColor: Colors.blue.withOpacity(.4),
+            // padding: EdgeInsets.all(8.0),
+            // decoration: BoxDecoration(
+            //   borderRadius: BorderRadius.circular(24.0),
+            //   color: Colors.grey[200],
+            // ),
+            child: _selectedReaction.isNotEmpty
+                ? Text(
+                    _selectedReaction,
+                    style: TextStyle(fontSize: 24.0),
+                  )
+                : Icon(Icons.thumb_up, color: Colors.blue),
+          ),
+        ),
+        _showReactions ? _buildReactions() : SizedBox(),
+      ],
+    );
+  }
+
+  Widget _buildReactions() {
+    return FadeTransition(
+      opacity: _scaleAnimation,
+      child: ScaleTransition(
+        scale: _scaleAnimation,
+        child: Container(
+          margin: EdgeInsets.only(top: 8.0),
+          padding: EdgeInsets.all(8.0),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8.0),
+            color: Colors.grey[200],
+          ),
+          child: Wrap(
+            spacing: 8.0,
+            children: _reactions.map((reaction) {
+              return GestureDetector(
+                onTap: () {
+                  setState(() {
+                    _selectedReaction = reaction;
+                    _hideReactions();
+                  });
+                },
+                child: Container(
+                  padding: EdgeInsets.all(4.0),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(12.0),
+                    color:
+                        _selectedReaction == reaction ? Colors.blue[100] : null,
+                  ),
+                  child: Text(
+                    reaction,
+                    style: TextStyle(fontSize: 24.0),
+                  ),
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showReactionsMenu() {
+    setState(() {
+      _showReactions = true;
+    });
+    _animationController.forward();
+  }
+
+  void _hideReactions() {
+    _animationController.reverse().then((value) {
+      setState(() {
+        _showReactions = false;
+      });
+    });
   }
 }
