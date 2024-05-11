acd0f984ba87a9ef2c9dd78344706a2949a32b43
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-05-11 10:58:06 +0300

    Add default Firebase options and handle notifications

--- lib/app/mogha.dart
+++ lib/app/mogha.dart
@@ -1,17 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:moga/core/bloc/global_cubit/app_language_cubit.dart';
-import 'package:moga/core/bloc/global_cubit/app_language_states.dart';
 import 'package:moga/core/common/connectivity.dart';
-import 'package:moga/core/common/screens/no_network_view.dart';
-import 'package:moga/core/local/app_local.dart';
-import 'package:moga/core/routes/app_routes.dart';
-import 'package:moga/core/services/service_locator.dart';
-import 'package:moga/core/theme/app_theme.dart';
-import 'package:moga/features/chats/presentation/manager/chats/chats_cubit.dart';
-import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
+import 'package:moga/core/common/start/there_is_a_connection.dart';
+import 'package:moga/core/common/start/there_is_no_connection.dart';
 
 class Mogha extends StatelessWidget {
   const Mogha({super.key});
@@ -25,54 +16,8 @@ class Mogha extends StatelessWidget {
           valueListenable: ConnectivityController.instance.isConnected,
           builder: (_, value, __) {
             return value
-                ? MultiBlocProvider(
-                    providers: [
-                      BlocProvider(
-                        create: (context) => sl<GlobalCubit>()..updateLangage(),
-                      ),
-                      BlocProvider(
-                        create: (context) => sl<ChatsCubit>()..getAllUsers(),
-                      ),
-                      BlocProvider(
-                        create: (context) => sl<SocialCubit>()
-                          ..getUserData()
-                          ..getPosts(),
-                      ),
-                    ],
-                    child: BlocBuilder<GlobalCubit, GlobalState>(
-                      builder: (context, state) => MaterialApp.router(
-                        localizationsDelegates: const [
-                          AppLocalizations.delegate,
-                          GlobalMaterialLocalizations.delegate,
-                          GlobalWidgetsLocalizations.delegate,
-                          GlobalCupertinoLocalizations.delegate,
-                        ],
-                        supportedLocales: const [
-                          Locale('en', "US"),
-                          Locale('ar', "EG"),
-                        ],
-                        locale: Locale(
-                          BlocProvider.of<GlobalCubit>(context).langCode,
-                        ),
-                        builder: (context, widget) {
-                          return Builder(
-                            builder: (context) {
-                              ConnectivityController.instance.initialize();
-                              return widget!;
-                            },
-                          );
-                        },
-                        debugShowCheckedModeBanner: false,
-                        routerConfig: AppRouter.route,
-                        theme: getAppDarkTheme(),
-                      ),
-                    ),
-                  )
-                : MaterialApp(
-                    title: 'No Network',
-                    debugShowCheckedModeBanner: false,
-                    home: const NoNetworkView(),
-                  );
+                ? const ThereIsAConnectionStart()
+                : const ThereIsNoconnection();
           },
         );
       },
--- lib/main.dart
+++ lib/main.dart
@@ -5,15 +5,20 @@ import 'package:moga/core/bloc/bloc_observer.dart';
 import 'package:moga/core/database/cache/cache_helper.dart';
 import 'package:moga/core/services/service_locator.dart';
 import "package:moga/core/widgets/error_used_when_you_don't_expect.dart";
+import 'package:moga/core/common/firebase/get_token&handle_notification.dart';
 import 'app/mogha.dart';
 import 'package:flutter/services.dart';
 
-import 'firebase_options.dart';
+import 'core/common/firebase/firebase_options.dart';
 
 void main() async {
   FlutterErrorWidget();
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await notificationHandler();
+  // ErrorWidget.builder = (FlutterErrorDetails details) {
+  //   return ErrorUsedWhenYouDonotExpect(details: details);
+  // };
   Bloc.observer = MyBlocObserver();
   await initServiceLocator();
   await sl<CacheHelper>().init();
