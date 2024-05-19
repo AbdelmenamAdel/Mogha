dbe9977e772fe2fda6c39704a3916f342fa445f9
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-05-19 12:25:36 +0300

    Refactor custom_video_picker.dart to simplify code and remove unnecessary if-else statement.

--- lib/core/widgets/custom_video_picker.dart
+++ lib/core/widgets/custom_video_picker.dart
@@ -2,9 +2,5 @@ import 'package:image_picker/image_picker.dart';
 
 Future<XFile?> pickVideo(ImageSource source) async {
   final XFile? video = await ImagePicker().pickVideo(source: source);
-  if (video != null) {
-    return video;
-  } else {
-    return null;
-  }
+  return video;
 }
