5d95d68c1bde1d0a78b7f1682532d29aaffc2feb
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-05-22 11:32:45 +0300

    Fix log out button and update settings view UI

--- lib/features/settings/presentation/views/settings.dart
+++ lib/features/settings/presentation/views/settings.dart
@@ -1,36 +1,90 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:icon_broken/icon_broken.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moga/core/bloc/global_cubit/app_language_cubit.dart';
+import 'package:moga/core/bloc/global_cubit/app_language_states.dart';
 import 'package:moga/core/utils/app_colors.dart';
-import 'package:moga/features/post/presentation/views/widgets/default_app_bar.dart';
+import 'package:moga/features/auth/data/models/create_user_model.dart';
 import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
-import 'package:moga/features/social/presentation/views/widgets/custom_app_bar.dart';
+
+import 'widgets/log_out.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var cubit = GlobalCubit.get(context);
-    return Scaffold(
-      body: Column(
-        children: [
-          DefaultAppBar(
-            title: 'Settings',
-            actions: [
-              IconButton(
-                onPressed: () {
-                  cubit.changeTheme();
-                },
-                icon: Icon(cubit.status),
-                color: cubit.status == CupertinoIcons.sun_max_fill
-                    ? Colors.yellowAccent
-                    : AppColors.kPrimary,
-              )
-            ],
-          ),
-        ],
+    var globalCubit = GlobalCubit.get(context);
+    UserModel model = SocialCubit.get(context).model!;
+    return SafeArea(
+      child: BlocConsumer<GlobalCubit, GlobalState>(
+        listener: (context, state) {},
+        builder: (context, state) {
+          return Scaffold(
+            appBar: AppBar(
+              leading: Text(''),
+              titleSpacing: -40,
+              title: Text(
+                'Settings',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleMedium!
+                    .copyWith(letterSpacing: 2),
+              ),
+              actions: [
+                IconButton(
+                  onPressed: () {
+                    globalCubit.changeTheme();
+                  },
+                  icon: Icon(globalCubit.status),
+                  color: globalCubit.status == CupertinoIcons.sun_max_fill
+                      ? Colors.yellowAccent
+                      : AppColors.kPrimary,
+                )
+              ],
+            ),
+            body: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  Card(
+                    elevation: 10,
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: Column(
+                        children: [
+                          GestureDetector(
+                            onTap: () {},
+                            child: ListTile(
+                                leading: CircleAvatar(
+                                  backgroundImage:
+                                      NetworkImage(model.profilePhoto),
+                                  radius: 25,
+                                ),
+                                title: Text(
+                                  model.userName,
+                                  style:
+                                      Theme.of(context).textTheme.displaySmall,
+                                ),
+                                trailing: LogOutBottomBar()),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Divider(
+                              height: 0,
+                              color: Theme.of(context).dividerColor,
+                            ),
+                          ),
+                          SizedBox(height: 5),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        },
       ),
     );
   }
---
+++ lib/features/settings/presentation/views/widgets/log_out.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:go_router/go_router.dart';
+import 'package:icon_broken/icon_broken.dart';
+import 'package:moga/features/post/presentation/views/widgets/default_app_bar.dart';
+
+class LogOutBottomBar extends StatelessWidget {
+  const LogOutBottomBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return CircleAvatar(
+      backgroundColor:
+          Theme.of(context).bottomNavigationBarTheme.backgroundColor,
+      child: IconButton(
+        onPressed: () {
+          showModalBottomSheet(
+            elevation: 5,
+            context: context,
+            clipBehavior: Clip.antiAliasWithSaveLayer,
+            builder: (context) => LogOutWidget(),
+          );
+        },
+        icon: Icon(IconBroken.Edit),
+      ),
+    );
+  }
+}
+
+class LogOutWidget extends StatelessWidget {
+  const LogOutWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.white,
+      height: 350.h,
+      width: double.infinity,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 12.0, vertical: 8),
+        child: Column(
+          children: [
+            ListTile(
+              leading: IconButton(
+                onPressed: () {},
+                icon: Icon(IconBroken.Logout),
+              ),
+            ),
+            Row(
+              children: [
+                Expanded(
+                  child: OutlinedButton(
+                    onPressed: () {
+                      GoRouter.of(context).pop();
+                    },
+                    child: Text('Cancel'),
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
