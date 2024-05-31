e2863214e002402ef607f98cad1a359b1dc93142
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-05-31 22:48:16 +0300

    Refactor settings view and add profile data section widget

--- lib/features/settings/presentation/views/settings.dart
+++ lib/features/settings/presentation/views/settings.dart
@@ -1,90 +1,118 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:hidable/hidable.dart';
 import 'package:moga/core/bloc/global_cubit/app_language_cubit.dart';
 import 'package:moga/core/bloc/global_cubit/app_language_states.dart';
+import 'package:moga/core/common/media_size.dart';
 import 'package:moga/core/utils/app_colors.dart';
+import 'package:moga/core/widgets/custom_divider.dart';
 import 'package:moga/features/auth/data/models/create_user_model.dart';
 import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
-
-import 'widgets/log_out.dart';
+import 'widgets/auther_media.dart';
+import 'widgets/profile_data_section.dart';
+import 'widgets/repeated_cards.dart';
 
 class SettingsView extends StatelessWidget {
   const SettingsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    var globalCubit = GlobalCubit.get(context);
-    UserModel model = SocialCubit.get(context).model!;
+    final UserModel model = SocialCubit.get(context).model!;
+    final ScrollController scrollController = ScrollController();
     return SafeArea(
       child: BlocConsumer<GlobalCubit, GlobalState>(
         listener: (context, state) {},
         builder: (context, state) {
           return Scaffold(
-            appBar: AppBar(
-              leading: Text(''),
-              titleSpacing: -40,
-              title: Text(
-                'Settings',
-                style: Theme.of(context)
-                    .textTheme
-                    .titleMedium!
-                    .copyWith(letterSpacing: 2),
-              ),
-              actions: [
-                IconButton(
-                  onPressed: () {
-                    globalCubit.changeTheme();
-                  },
-                  icon: Icon(globalCubit.status),
-                  color: globalCubit.status == CupertinoIcons.sun_max_fill
-                      ? Colors.yellowAccent
-                      : AppColors.kPrimary,
-                )
-              ],
-            ),
+            appBar: appBar(context, scrollController),
             body: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  Card(
-                    elevation: 10,
-                    child: Padding(
-                      padding: const EdgeInsets.all(4.0),
-                      child: Column(
-                        children: [
-                          GestureDetector(
-                            onTap: () {},
-                            child: ListTile(
-                                leading: CircleAvatar(
-                                  backgroundImage:
-                                      NetworkImage(model.profilePhoto),
-                                  radius: 25,
-                                ),
-                                title: Text(
-                                  model.userName,
-                                  style:
-                                      Theme.of(context).textTheme.displaySmall,
-                                ),
-                                trailing: LogOutBottomBar()),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Divider(
-                              height: 0,
-                              color: Theme.of(context).dividerColor,
-                            ),
-                          ),
-                          SizedBox(height: 5),
-                        ],
-                      ),
+              padding: const EdgeInsets.all(5.0),
+              child: SingleChildScrollView(
+                controller: scrollController,
+                child: Column(
+                  children: [
+                    ProfileDataInSection(),
+                    SizedBox(height: 10),
+                    ProfileHeaderLabel(headerLabel: 'Account Info'),
+                    RepeatedCards(
+                      email: model.email,
                     ),
-                  )
-                ],
+                    AuthorMedia(),
+                  ],
+                ),
               ),
             ),
           );
         },
+      ),
+    );
+  }
+
+  PreferredSizeWidget? appBar(context, scrollController) {
+    var globalCubit = GlobalCubit.get(context);
+    return Hidable(
+      controller: scrollController,
+      child: AppBar(
+        leading: Text(''),
+        elevation: 1,
+        shadowColor: AppColors.grey,
+        titleSpacing: -40,
+        title: Text(
+          'Settings',
+          style: Theme.of(context)
+              .textTheme
+              .titleMedium!
+              .copyWith(letterSpacing: 2),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              globalCubit.changeTheme();
+            },
+            icon: Icon(globalCubit.status),
+            color: globalCubit.status == CupertinoIcons.sun_max_fill
+                ? Colors.yellowAccent
+                : AppColors.kPrimary,
+          )
+        ],
+      ),
+    );
+  }
+}
+class ProfileHeaderLabel extends StatelessWidget {
+  final String headerLabel;
+
+  const ProfileHeaderLabel({super.key, required this.headerLabel});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 35.h,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 35.h,
+            width: 105.w,
+            child: CustomDivider()
+          ),
+          Text(
+            headerLabel,
+            style: TextStyle(
+              fontFamily: 'Dosis',
+              // color: textColor,
+              fontSize: 23.sp,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          SizedBox(
+            height: 35.h,
+            width: 85.w,
+            child: CustomDivider(),
+          ),
+        ],
       ),
     );
   }
---
+++ lib/features/settings/presentation/views/widgets/profile_data_section.dart
@@ -0,0 +1,68 @@
+
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:icon_broken/icon_broken.dart';
+import 'package:moga/core/routes/app_routes.dart';
+import 'package:moga/core/widgets/custom_divider.dart';
+import 'package:moga/features/auth/data/models/create_user_model.dart';
+import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
+
+import 'log_out.dart';
+
+class ProfileDataInSection extends StatelessWidget {
+  const ProfileDataInSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final UserModel model = SocialCubit.get(context).model!;
+    return Card(
+      elevation: 10,
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 8.0),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                GoRouter.of(context).push(AppRoutes.profile);
+              },
+              child: ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(model.profilePhoto),
+                  radius: 25,
+                ),
+                title: Text(
+                  model.userName,
+                  style: Theme.of(context).textTheme.displaySmall,
+                ),
+                trailing: LogOutBottomBar(),
+              ),
+            ),
+            CustomDivider(),
+            InkWell(
+              onTap: () {},
+              child: Padding(
+                padding: const EdgeInsets.all(12.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      IconBroken.Plus,
+                      size: 28,
+                    ),
+                    SizedBox(
+                      width: 12,
+                    ),
+                    Text(
+                      'Create a new account ',
+                      style: Theme.of(context).textTheme.displaySmall,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
