b4c18b3dba2fa7fbc63e0c4e134199de36b5ef62
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-06-07 23:34:22 +0300

    Add line breaks and fix typos in Constants class description

--- lib/core/common/constants/constants.dart
+++ lib/core/common/constants/constants.dart
@@ -1,7 +1,7 @@
 class Constants {
   static String aboutMogha =
       "Social media application that allows users to connect with their friends and follow them all around the world. "
-      "It provides a convenient way for users to connect,share their posts,stories,chat with friens,change theme and language too with all encrypted end to end data. "
+      "\n\nIt provides a convenient way for users to connect,share their posts,stories,chat with friens,change theme and language too with all encrypted end to end data. "
       "\n\nThe application features a wide selection of Authentication,very good User Interface approved continuously for mogha users,and more features like posts,search,chatting,and more. "
       "Users can browse through different devices to connect their accounts."
       "\n\nThe application also made with a proffesional architecture to be able to grow up any sec "
