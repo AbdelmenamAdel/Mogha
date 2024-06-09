e71af83bc7037526489f831b8cf1f28ebe4c0631
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-06-09 13:35:26 +0300

    chats view

--- lib/features/chats/chats_view.dart
+++ lib/features/chats/chats_view.dart
@@ -1,7 +1,11 @@
+import 'dart:developer';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:moga/core/services/service_locator.dart';
 import 'package:moga/features/chats/presentation/manager/chats/chats_cubit.dart';
 import 'package:moga/features/chats/presentation/manager/chats/chats_states.dart';
+import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
 import 'package:moga/features/social/presentation/views/widgets/custom_app_bar.dart';
 import 'chats_view_body.dart';
 
@@ -13,12 +17,29 @@ class ChatsView extends StatelessWidget {
     return BlocConsumer<ChatsCubit, ChatsStates>(
       listener: (context, state) {},
       builder: (context, state) {
+        List<String> usersId = [];
+
+        void fetchChatsForUser(String userId) async {
+          // Reference to the specific user's 'chats' sub-collection
+          var chatsRef = await FirebaseFirestore.instance
+              .collection('users')
+              .doc(userId)
+              .collection('chats')
+              .get();
+          chatsRef.docs.forEach((element) {
+            log(element.toString());
+          });
+        }
+
+        fetchChatsForUser(sl<SocialCubit>().model!.uId);
         return SafeArea(
           child: Scaffold(
             body: Column(
               children: [
                 CustomAppBar(title: 'Chats'),
-                ChatsViewBody(),
+                ChatsViewBody(
+                  chatList: usersId,
+                ),
               ],
             ),
           ),
