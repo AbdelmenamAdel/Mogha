cd4ce4ea3624125e8cc2f04a569bba9ec5beaa1a
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-06-09 19:18:40 +0300

    add drawer in home and

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,30 +1,104 @@
 import 'package:flutter/material.dart';
 import 'package:hidable/hidable.dart';
 import 'package:moga/features/social/presentation/views/widgets/custom_app_bar.dart';
-
+import 'widgets/custom_drawer.dart';
 import 'widgets/home_view_body.dart';
 
-class HomeView extends StatelessWidget {
+class HomeView extends StatefulWidget {
   const HomeView({
     super.key,
   });
 
+  @override
+  State<HomeView> createState() => _HomeViewState();
+}
+
+class _HomeViewState extends State<HomeView> with TickerProviderStateMixin {
+  late AnimationController _animationController;
+  late Animation<double> _animation1;
+  late Animation<double> _animation2;
+  late Animation<double> _animation3;
+
+  bool _bool = true;
+
+  @override
+  void initState() {
+    super.initState();
+    initAnimationControllers();
+  }
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
-    final ScrollController scrollController=ScrollController();
+    final ScrollController scrollController = ScrollController();
     return SafeArea(
       child: Scaffold(
         appBar: Hidable(
           controller: scrollController,
           child: CustomAppBar(
+            iconButton: IconButton(
+              onPressed: () {
+                if (_bool == true) {
+                  _animationController.forward();
+                } else {
+                  _animationController.reverse();
+                }
+                _bool = false;
+              },
+              icon: Icon(Icons.menu_rounded),
+            ),
             title: 'Home',
           ),
         ),
-        body: HomeViewBody(
-          scrollController: scrollController,
+        body: Stack(
+          children: [
+            HomeViewBody(
+              scrollController: scrollController,
+            ),
+            CustomDrawer(
+              condition: _bool,
+              animation1: _animation1,
+              animation2: _animation2,
+              animation3: _animation3,
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void initAnimationControllers() {
+    _animationController =
+        AnimationController(vsync: this, duration: Duration(milliseconds: 600));
+
+    _animation1 = Tween<double>(begin: 0, end: 20).animate(CurvedAnimation(
+      parent: _animationController,
+      curve: Curves.easeOut,
+      reverseCurve: Curves.easeIn,
+    ))
+      ..addListener(() {
+        setState(() {});
+      })
+      ..addStatusListener((status) {
+        if (status == AnimationStatus.dismissed) {
+          _bool = true;
+        }
+      });
+    _animation2 = Tween<double>(begin: 0, end: .3).animate(_animationController)
+      ..addListener(() {
+        setState(() {});
+      });
+    _animation3 = Tween<double>(begin: .9, end: 1).animate(CurvedAnimation(
+        parent: _animationController,
+        curve: Curves.fastLinearToSlowEaseIn,
+        reverseCurve: Curves.ease))
+      ..addListener(() {
+        setState(() {});
+      });
   }
 }
---
+++ lib/features/home/presentation/views/widgets/custom_drawer.dart
@@ -0,0 +1,124 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:icon_broken/icon_broken.dart';
+import 'package:moga/core/common/custom_navigate.dart';
+import 'package:moga/core/routes/app_routes.dart';
+import 'package:moga/core/services/service_locator.dart';
+import 'package:moga/core/widgets/custom_divider.dart';
+import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
+
+import 'repeated_list_in_drawer.dart';
+
+class CustomDrawer extends StatelessWidget {
+  const CustomDrawer({
+    super.key,
+    required this.condition,
+    required this.animation1,
+    required this.animation2,
+    required this.animation3,
+  });
+  final Animation<double> animation1;
+  final Animation<double> animation2;
+  final Animation<double> animation3;
+
+  final bool condition;
+  @override
+  Widget build(BuildContext context) {
+    double _height = MediaQuery.of(context).size.height;
+    double _width = MediaQuery.of(context).size.width;
+
+    return BackdropFilter(
+      filter:
+          ImageFilter.blur(sigmaY: animation1.value, sigmaX: animation1.value),
+      child: Container(
+        height: condition ? 0 : _height,
+        width: condition ? 0 : _width,
+        color: Colors.transparent,
+        child: Center(
+          child: Transform.scale(
+            scale: animation3.value,
+            child: Container(
+              width: _width * .9,
+              height: _width * 1.2,
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(animation2.value),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              child: CustomRepeatedRows(),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomRepeatedRows extends StatelessWidget {
+  const CustomRepeatedRows({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          CircleAvatar(
+            backgroundImage:
+                NetworkImage(sl<SocialCubit>().model?.profilePhoto ?? ''),
+            radius: 35,
+          ),
+          Text(
+            sl<SocialCubit>().model?.userName ?? '',
+            style: TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          CustomDivider(),
+          RepeatedListTailInDrawer(
+            icon: Icons.person_outline_rounded,
+            title: 'Profile',
+            onTap: () {
+              HapticFeedback.lightImpact();
+              context.navigate(AppRoutes.profile, context);
+            },
+          ),
+          CustomDivider(),
+          RepeatedListTailInDrawer(
+            icon: Icons.add_a_photo_outlined,
+            title: 'Add Story',
+            onTap: () {
+              HapticFeedback.lightImpact();
+            },
+          ),
+          CustomDivider(),
+          RepeatedListTailInDrawer(
+            icon: IconBroken.Paper_Upload,
+            title: 'Add Post',
+            onTap: () {
+              HapticFeedback.lightImpact();
+              context.navigate(AppRoutes.newPost, context);
+            },
+          ),
+          CustomDivider(),
+          RepeatedListTailInDrawer(
+            icon: Icons.settings_outlined,
+            title: 'Settings',
+            onTap: () {
+              HapticFeedback.lightImpact();
+              sl<SocialCubit>().changeBottomNav(3);
+            },
+          ),
+          CustomDivider(),
+          RepeatedListTailInDrawer(
+            icon: Icons.info_outline_rounded,
+            title: 'About',
+            onTap: () {
+              HapticFeedback.lightImpact();
+              context.navigate(AppRoutes.about, context);
+            },
+          ),
+        ]);
+  }
+}
