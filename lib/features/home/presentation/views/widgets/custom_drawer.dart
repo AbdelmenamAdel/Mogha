b9aad0efa1446cf986cd058d62860b9ffea7d234
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-06-11 14:53:44 +0300

    add drawer to home

--- lib/features/home/presentation/views/widgets/custom_drawer.dart
+++ lib/features/home/presentation/views/widgets/custom_drawer.dart
@@ -7,7 +7,6 @@ import 'package:moga/core/routes/app_routes.dart';
 import 'package:moga/core/services/service_locator.dart';
 import 'package:moga/core/widgets/custom_divider.dart';
 import 'package:moga/features/social/presentation/manager/social_cubit/social_cubit.dart';
-
 import 'repeated_list_in_drawer.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -40,7 +39,7 @@ class CustomDrawer extends StatelessWidget {
             scale: animation3.value,
             child: Container(
               width: _width * .9,
-              height: _width * 1.2,
+              height: _width * 1.3,
               decoration: BoxDecoration(
                 color: Colors.white.withOpacity(animation2.value),
                 borderRadius: BorderRadius.circular(30),
@@ -59,66 +58,73 @@ class CustomRepeatedRows extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          CircleAvatar(
-            backgroundImage:
-                NetworkImage(sl<SocialCubit>().model?.profilePhoto ?? ''),
-            radius: 35,
-          ),
-          Text(
-            sl<SocialCubit>().model?.userName ?? '',
-            style: TextStyle(
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 12.0, horizontal: 5),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            CircleAvatar(
+              backgroundImage:
+                  NetworkImage(sl<SocialCubit>().model?.profilePhoto ?? ''),
+              radius: 35,
             ),
-          ),
-          CustomDivider(),
-          RepeatedListTailInDrawer(
-            icon: Icons.person_outline_rounded,
-            title: 'Profile',
-            onTap: () {
-              HapticFeedback.lightImpact();
-              context.navigate(AppRoutes.profile, context);
-            },
-          ),
-          CustomDivider(),
-          RepeatedListTailInDrawer(
-            icon: Icons.add_a_photo_outlined,
-            title: 'Add Story',
-            onTap: () {
-              HapticFeedback.lightImpact();
-            },
-          ),
-          CustomDivider(),
-          RepeatedListTailInDrawer(
-            icon: IconBroken.Paper_Upload,
-            title: 'Add Post',
-            onTap: () {
-              HapticFeedback.lightImpact();
-              context.navigate(AppRoutes.newPost, context);
-            },
-          ),
-          CustomDivider(),
-          RepeatedListTailInDrawer(
-            icon: Icons.settings_outlined,
-            title: 'Settings',
-            onTap: () {
-              HapticFeedback.lightImpact();
-              sl<SocialCubit>().changeBottomNav(3);
-            },
-          ),
-          CustomDivider(),
-          RepeatedListTailInDrawer(
-            icon: Icons.info_outline_rounded,
-            title: 'About',
-            onTap: () {
-              HapticFeedback.lightImpact();
-              context.navigate(AppRoutes.about, context);
-            },
-          ),
-        ]);
+            Text(
+              sl<SocialCubit>().model?.userName ?? '',
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            CustomDivider(),
+            RepeatedListTailInDrawer(
+              icon: Icons.person_outline_rounded,
+              title: 'Profile',
+              onTap: () {
+                HapticFeedback.lightImpact();
+                context.navigate(AppRoutes.profile, context);
+              },
+            ),
+            CustomDivider(),
+            RepeatedListTailInDrawer(
+              icon: Icons.add_a_photo_outlined,
+              title: 'Add Story',
+              onTap: () {
+                HapticFeedback.lightImpact();
+                context.navigate(AppRoutes.addStory, context);
+              },
+            ),
+            CustomDivider(),
+            RepeatedListTailInDrawer(
+              icon: IconBroken.Paper_Upload,
+              title: 'Add Post',
+              onTap: () {
+                HapticFeedback.lightImpact();
+                context.navigate(AppRoutes.newPost, context);
+              },
+            ),
+            CustomDivider(),
+            RepeatedListTailInDrawer(
+              icon: Icons.settings_outlined,
+              title: 'Settings',
+              onTap: () {
+                HapticFeedback.lightImpact();
+                sl<SocialCubit>().changeBottomNav(3);
+              },
+            ),
+            CustomDivider(),
+            RepeatedListTailInDrawer(
+              icon: Icons.info_outline_rounded,
+              title: 'About',
+              onTap: () {
+                HapticFeedback.lightImpact();
+                context.navigate(AppRoutes.about, context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
