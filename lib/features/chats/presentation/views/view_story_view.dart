70b3de52980d189d27e914bfd486053e00b7059c
AbdelmenamAdel <abdelmenamadel96@gmail.com>
2024-06-11 14:53:22 +0300

    add screen to view story

---
+++ lib/features/chats/presentation/views/view_story_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewStoryView extends StatelessWidget {
+  const ViewStoryView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
