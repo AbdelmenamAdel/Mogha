9fa6fe8323dd3af542815ad81e5a6c596e26ed08
Abdelmoneim Adel <abdelmenamadel96@gmail.com>
2024-07-07 01:57:09 +0200

    app lang

--- lib/core/bloc/global_cubit/app_language_cubit.dart
+++ lib/core/bloc/global_cubit/app_language_cubit.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moga/core/database/cache/cache_helper.dart';
 import '../../services/service_locator.dart';
